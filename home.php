8e675d8dfa0bc52ff1d4a4ca032d4f4788e9bb2e
Aymeric Le Feyer <lefeyer.aymeric@gmail.com>
2020-10-01 17:11:30 +0200

    test php

--- home.php
+++ home.php
@@ -72,90 +72,7 @@
     </div>
     <!-- end of preloader -->
 
-    <!-- Navbar -->
-    <nav class="navbar navbar-expand-md navbar-dark navbar-custom fixed-top">
-      <!-- Text Logo - Use this if you don't have a graphic logo -->
-      <!-- <a class="navbar-brand logo-text page-scroll" href="index.html">Aria</a> -->
-
-      <!-- Image Logo -->
-      <a class="navbar-brand logo-image" href="home.html"
-        ><img src="images/logo.png" alt="logo"
-      /></a>
-
-      <!-- Mobile Menu Toggle Button -->
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarsExampleDefault"
-        aria-controls="navbarsExampleDefault"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-awesome fas fa-bars"></span>
-        <span class="navbar-toggler-awesome fas fa-times"></span>
-      </button>
-      <!-- end of mobile menu toggle button -->
-
-      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item">
-            <a class="nav-link page-scroll" href="#header"
-              >ACCUEIL <span class="sr-only">(current)</span></a
-            >
-          </li>
-          <li class="nav-item">
-            <a class="nav-link page-scroll" href="#intro">INTRO</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link page-scroll" href="#services">FORMATION</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link page-scroll" href="#skills">COMPÉTENCES</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link page-scroll" href="#experiences">EXPÉRIENCES</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link page-scroll" href="#projects">PROJETS</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link page-scroll" href="#contact">CONTACT</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-    <!-- end of navbar -->
-    <!-- end of navbar -->
-
-    <!-- Header -->
-    <header id="header" class="header">
-      <div class="header-content">
-        <div class="container">
-          <div class="row">
-            <div class="col-lg-12">
-              <div class="text-container">
-                <h1>
-                  DÉVELOPPEUR
-                  <span id="js-rotating">MOBILE, FRONT-END, BACK-END</span>
-                </h1>
-                <p class="p-heading p-large">
-                  Je suis un jeune développeur fullstack avec une perpétuelle
-                  envie de progresser et une soif de connaissance inarrêtable
-                </p>
-                <a class="btn-solid-lg page-scroll" href="#intro">DÉCOUVRIR</a>
-              </div>
-            </div>
-            <!-- end of col -->
-          </div>
-          <!-- end of row -->
-        </div>
-        <!-- end of container -->
-      </div>
-      <!-- end of header-content -->
-    </header>
-    <!-- end of header -->
-    <!-- end of header -->
+    <?php include('header.html'); ?>
 
     <!-- Intro -->
     <div id="intro" class="basic-1">
@@ -599,7 +516,7 @@
             C'est avec Flutter que j'ai pu réaliser mon premier vrai projet un
             peu sérieux, j'ai nommé <a href="#"><strong>Bonap</strong></a
             >. <br />
-            Avec mon coéquipier <a>Quentin Carry</a>, nous avons su apprendre à
+            Avec mon coéquipier <a href="https://www.linkedin.com/in/quentincarry/" target="_blank">Quentin Carry</a>, nous avons su apprendre à
             utiliser ce framwork, et en même temps à travailler en équipe. Nous
             avons su livrer une solution propre dans le cadre du projet de fin
             de licence
@@ -1181,7 +1098,7 @@
                   >
                 </li>
                 <li>
-                  <a class="white" href="#your-link" target="_blank"
+                  <a class="white" href="https://www.linkedin.com/in/quentincarry/" target="_blank"
                     >Quentin CARRY</a
                   >
                 </li>
