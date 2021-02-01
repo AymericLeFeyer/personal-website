21308652ffdeb916a89f318df1e75dcd611b7054
Aymeric Le Feyer <lefeyer.aymeric@gmail.com>
2021-02-01 22:35:15 +0100

    vuejs reactjs

---
+++ components/skills/reactjs.php
@@ -0,0 +1,48 @@
+<!-- Item -->
+<div class="element-item ...">
+    <a class="popup-with-move-anim" href="#skill-reactjs">
+        <div class="element-item-overlay"><span>ReactJS</span></div>
+        <img src="images/skills/reactjs.png" alt="reactjs" />
+    </a>
+</div>
+
+<!-- Lightbox -->
+<div id="skill-reactjs" class="lightbox-basic zoom-anim-dialog mfp-hide">
+    <div class="row">
+        <button title="Close (Esc)" type="button" class="mfp-close x-button">
+            ×
+        </button>
+
+        <div class="col-lg-12">
+            <h3>ReactJS</h3>
+            <hr class="line-heading" />
+            <h6>Niveau débutant</h6>
+            <p>
+                Pendant le confinement (le premier du nom), j'ai découvert ReactJS. J'ai commencé par créer un memory et
+                un petit jeu du pendu, rien de fou mais suffisant pour passer le temps
+            </p>
+            <p>
+                Pour rejoindre l'équipe de <a class="popup-with-move-anim"
+                    href=#exp-pit><strong>Progress-IT</strong></a>, j'ai dû passer un test technique. La technologie
+                étant au choix, j'ai choisi ReactJS pour le challenge</p>
+            <h6 class="mb-3">Utilisations de ReactJS :</h6>
+            <ul>
+                <li>Mini-jeux (pendu, memory)</li>
+                <li>Test technique <a class="popup-with-move-anim" href=#exp-pit>Progress-IT</a></li>
+
+            </ul>
+
+            <div class="testimonial-container">
+                <p class="testimonial-text">
+                    ReactJS c'est marrant, c'est mon premier framework web. Débutant sur cette technologie, je ne dirais
+                    pas non à approfondir mes connaissances sur ReactJS
+                </p>
+
+            </div>
+        </div>
+        <!-- end of col -->
+    </div>
+    <!-- end of row -->
+</div>
+<!-- end of lightbox-basic -->
+<!-- end of lightbox -->
