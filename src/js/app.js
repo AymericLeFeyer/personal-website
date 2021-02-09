fdfcd20ceb00e95461370db2f87d9cd188051b44
Aymeric Le Feyer <lefeyer.aymeric@gmail.com>
2021-02-09 15:45:09 +0100

    fonts

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,1 @@
 new WOW().init();
-
-$(".titlePage").one(
-  "animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd",
-  function () {
-    $(this).css("display", "none");
-  }
-);
--- src/js/fullpage.js
+++ src/js/fullpage.js
@@ -4,6 +4,15 @@ console.warn(
 );
 new fullpage("#fullpage", {
   anchors: ["home", "tech", "portfolio", "experiences", "studies", "contact"],
-  sectionsColor: ["yellow", "orange", "#C0C0C0", "#ADD8E6", "blue", "green"],
+  sectionsColor: [
+    "#78C0E0",
+    "#009DD1",
+
+    "#ADD8E6",
+    "#ADE8FF",
+
+    "#B0D7FF",
+    "#BBDEF0",
+  ],
   scrollBar: true,
 });
