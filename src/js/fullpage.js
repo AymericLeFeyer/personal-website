2aaa335dd6a5b8ea281dddd73aa06ca14156bb4b
Aymeric Le Feyer <lefeyer.aymeric@gmail.com>
2021-02-09 13:33:23 +0100

    pages skeleton

--- src/js/fullpage.js
+++ src/js/fullpage.js
@@ -3,6 +3,7 @@ console.warn(
   "The following error has no sense cause the project is open source !"
 );
 new fullpage("#fullpage", {
-  anchors: ["home", "tech", "studies", "experiences"],
-  sectionsColor: ["yellow", "orange", "#C0C0C0", "#ADD8E6"],
+  anchors: ["home", "tech", "portfolio", "experiences", "studies", "contact"],
+  sectionsColor: ["yellow", "orange", "#C0C0C0", "#ADD8E6", "blue", "green"],
+  scrollBar: true,
 });
