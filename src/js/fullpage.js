9ea06faf9db41dff95708649d345ae81b299f0f7
Aymeric Le Feyer <lefeyer.aymeric@gmail.com>
2021-02-09 10:20:31 +0100

    init

---
+++ src/js/fullpage.js
@@ -0,0 +1,8 @@
+// Fullpage
+console.warn(
+  "The following error has no sense cause the project is open source !"
+);
+new fullpage("#fullpage", {
+  anchors: ["home", "tech", "studies", "experiences"],
+  sectionsColor: ["yellow", "orange", "#C0C0C0", "#ADD8E6"],
+});
