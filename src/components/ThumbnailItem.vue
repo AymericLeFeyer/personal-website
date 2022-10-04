41d8f2f67d63f75949a341b4d61414b5c743622d
Aymeric Le Feyer <aymeric.lefeyer@progress-it.fr>
2022-10-04 22:16:45 +0200

    feat(theme): shadow color

--- src/components/ThumbnailItem.vue
+++ src/components/ThumbnailItem.vue
@@ -52,9 +52,10 @@ export default {
   width: 240px;
   border-radius: 5px;
   transition: all 0.2s ease-in-out;
-  box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.2);
+  box-shadow: 0 0 10px 0 $shadow;
+
   &:hover {
-    transform: scale(1.02);
+    box-shadow: 0 0 20px 0 $shadow;
   }
   .image {
     img {
