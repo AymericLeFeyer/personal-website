efcec6725cdd585c890f1e4c33aa52b04e20029a
Aymeric Le Feyer <aymeric.lefeyer@progress-it.fr>
2022-10-08 10:27:08 +0200

    style(theme-color): listner

--- src/App.vue
+++ src/App.vue
@@ -13,21 +13,29 @@ export default {
     Breadcrumb,
   },
   mounted() {
+    this.setTheme();
+
     window
       .matchMedia("(prefers-color-scheme: dark)")
       .addEventListener("change", (event) => {
-        if (event.matches) {
-          //dark theme
-          document
-            .querySelector('meta[name="theme-color"]')
-            .setAttribute("content", "#20211F");
-        } else {
-          //light theme
-          document
-            .querySelector('meta[name="theme-color"]')
-            .setAttribute("content", "#f3f3f3");
-        }
+        this.setTheme();
       });
+  },
+
+  methods: {
+    setTheme() {
+      if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+        //dark theme
+        document
+          .querySelector('meta[name="theme-color"]')
+          .setAttribute("content", "#20211F");
+      } else {
+        //light theme
+        document
+          .querySelector('meta[name="theme-color"]')
+          .setAttribute("content", "#f3f3f3");
+      }
+    },
   },
 };
 </script>
